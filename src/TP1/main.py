93852c05c9a0f6a409ff37e1b77a842d33363f84
JoaoESmoreira <jesmoreira2010@gmail.com>
2023-02-12 00:28:00 +0000

    split and visualise channels with colormap

--- src/TP1/main.py
+++ src/TP1/main.py
@@ -19,15 +19,43 @@ class jpeg:
             print(self.data)
 
 
-    def showImage(self):
+    def showImage(self, *args):
         plt.figure()
-        plt.imshow(self.data)
+        if len(args) > 0:
+            plt.imshow(args[0])
+        else:
+            plt.imshow(self.data)
         plt.axis('off')
         plt.show()
+    
+    def showColorMap(self, data, colorMap):
+        plt.figure()
+        plt.axis('off')
+        plt.imshow(data, colorMap)
+        plt.show()
+
+    def splitChannels(self):
+        self.R = self.data[:, :, 0]
+        self.G = self.data[:, :, 1]
+        self.B = self.data[:, :, 2]
+
+    def merge_channels(self):
+        return  np.dstack((self.R,self.G,self.B))
+
+    def showChannels(self):
+        red = self.gradient('myRed', (1,0,0))
+        green = self.gradient('myGreen', (0,1,0))
+        blue = self.gradient('myBlue', (0,0,1))
+
+        self.splitChannels()
+
+        self.showColorMap(self.R, red)
+        self.showColorMap(self.G, green)
+        self.showColorMap(self.B, blue)
 
 
-    def gradient(self, color):
-        return clr.LinearSegmentedColormap.from_list('custom', [(0,0,0), (0,1, 0)], 256)
+    def gradient(self, name, color=(1,1,1)):
+        return clr.LinearSegmentedColormap.from_list(name, [(0,0,0), color], 256)
 
     def encoder(self):
         pass
@@ -41,5 +69,4 @@ if __name__ == "__main__":
     a = jpeg('../../Assets/peppers/peppers.bmp')
     a.readImage()
     a.showImage()
-    # g = a.gradient("blue")
-    # print(type(g))
+    a.showChannels()
