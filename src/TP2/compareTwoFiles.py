03dcdb10eb6c028a6a0c71722295773d66f61b9e
Tomás Pinto <tomascerveira10@gmail.com>
2023-04-25 18:42:31 +0100

    3 done

---
+++ src/TP2/compareTwoFiles.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+class file():
+
+    def __init__(self, path):
+        self.file = self.__readFile(path)
+
+
+    def __readFile(self, path):
+        np.set_printoptions(suppress=True)
+        return np.genfromtxt(path, delimiter=',')
+    
+
+    def getData(self):
+        return self.file
+
+    
+    def compare(self, dataFile):
+        # diff = np.zeros(np.shape(self.file))
+        diff = np.abs(self.file - dataFile) > 0.1
+
+        return diff
+    
+    
+
+if __name__ == "__main__":
+    a = file('featuresStates.csv')
+    b = file('notNormFMrosa.csv')
+
+    output = a.compare(b.getData()[0])
+    print(np.shape(output))
+    for i in range(len(output[0])):
+        if output[0, i] == True:
+            print(i, end=" ")
+
+    print("\n")
--- src/TP2/main.py
+++ src/TP2/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import librosa
 import scipy 
+from scipy.spatial.distance import cityblock , cosine
 
 from os import listdir
 from os.path import isfile, join
@@ -16,17 +17,25 @@ class feature():
         self.fmin = 20
         self.fmax = self.sr // 2
 
-        self.features = self.__readTop100features(path)
-        self.features = self.__normalization(0, 1, self.features)
-        self.__saveFeatures("./feature.csv", self.features)
+        #self.features = self.__readTop100features(path)
+        #self.features = self.__normalization(0, 1, self.features)
+        #self.__saveFeatures("./feature.csv", self.features)
+        self.features = self.__readFile('./feature.csv')
 
-        # self.__readDirectory("./assets/songs/")
-        # self.__getFeatures("./assets/songs/")
-        # self.__saveFeatures("./featuresStates.csv", self.featuresStats)
+        self.__readDirectory("./assets/songs/")
+        self.__readDirectory2("./assets/Queries/")
+        #self.__getFeatures("./assets/songs/")
+        #self.__saveFeatures("./featuresStates.csv", self.featuresStats)
 
         self.featuresStats = self.__readFile('./featuresStates.csv')
-        self.featuresStatsNormalizated = self.__normalization(0, 1, self.featuresStats)
-        self.__saveFeatures("./featuresStatesNormalizated.csv", self.featuresStatsNormalizated)
+        self.featuresStatsNormalizated = self.__readFile('./featuresStatesNormalizated.csv')
+        #self.__saveFeatures("./featuresStatesNormalizated.csv", self.featuresStatsNormalizated)
+        #self.featuresStatsNormalizated = self.__normalization(0, 1, self.featuresStats)
+        #self.__saveFeatures("./featuresStatesNormalizated.csv", self.featuresStats)
+
+        self.metricasSim()
+        for song in range(len(self.songsNames2)):
+            self.ranking(self.songsNames2[song])
 
 
     def __readTop100features(self, path):
@@ -50,42 +59,37 @@ class feature():
 
             if fMax == fMin:
                 data[:, column] = 0
+            else:
+                try:
+                    data[:, column] = a + ((data[:, column]-fMin) * (b-a)) / (fMax-fMin)
+                except:
+                    print(data[:, column])
 
-            try:
-                data[:, column] = a + ((data[:, column]-fMin) * (b-a)) / (fMax-fMin)
-            except:
-                print(data[:, column])
-
-
-        return np.round(data, 5)
+        return data
 
 
     def __saveFeatures(self, path, data):
         # self.showFeatures()
-        np.savetxt(path, data, delimiter=',')
+        np.savetxt(path, data, fmt="%lf", delimiter=',')
 
 
     def __readDirectory(self, path):
         self.songsNames = [f for f in listdir(path) if isfile(join(path, f))]
 
+    def __readDirectory2(self, path):
+        self.songsNames2 = [f for f in listdir(path) if isfile(join(path, f))]
+
 
     def extrationStats(self, data):
         mean = np.mean(data)
-        stddv = scipy.stats.tstd(data)
+        stddv = np.std(data)
         skew = scipy.stats.skew(data)
         kurtosis = scipy.stats.kurtosis(data)
         median = np.median(data)
         max = np.max(data)
         min = np.min(data)
-        # print(mean)
-        # print(stddv)
-        # print(skew)
-        # print(kurtosis)
-        # print(median)
-        # print(max)
-        # print(min)
         
-        return np.array([mean, stddv, skew, kurtosis, median, min, max], dtype=object)
+        return np.array([mean, stddv, skew, kurtosis, median, max, min], dtype=object)
 
 
     def __getFeatures(self, path):
@@ -146,6 +150,80 @@ class feature():
             print(row)
             break
     
+    def metricasSim(self):
+
+        self.euclidian100 = np.arange(900*900, dtype=object).reshape((900, 900))
+        self.manhattan100 = np.arange(900*900, dtype=object).reshape((900, 900))
+        self.cosine100 = np.arange(900*900, dtype=object).reshape((900, 900))
+
+        self.euclidianF = np.arange(900*900, dtype=object).reshape((900, 900))
+        self.manhattanF = np.arange(900*900, dtype=object).reshape((900, 900))
+        self.cosineF = np.arange(900*900, dtype=object).reshape((900, 900))
+
+        for i in range(900):
+            for j in range(900):
+                self.euclidian100[i][j] = np.linalg.norm(self.features[i] - self.features[j])
+                self.manhattan100[i][j] = cityblock(self.features[i], self.features[j])
+                self.cosine100[i][j] = cosine(self.features[i],self.features[j])
+
+                self.euclidianF[i][j] = np.linalg.norm(self.featuresStatsNormalizated[i] - self.featuresStatsNormalizated[j])
+                self.manhattanF[i][j] = cityblock(self.featuresStatsNormalizated[i], self.featuresStatsNormalizated[j])
+                self.cosineF[i][j] = cosine(self.featuresStatsNormalizated[i],self.featuresStatsNormalizated[j])
+        
+        self.__saveFeatures("./euclidian_top100.csv", self.euclidian100)
+        self.__saveFeatures("./euclidian_features.csv", self.euclidianF)
+        
+        self.__saveFeatures("./manhattan_top100.csv", self.manhattan100)
+        self.__saveFeatures("./manhattan_features.csv", self.manhattanF)
+        
+        self.__saveFeatures("./cosine_top100.csv", self.cosine100)
+        self.__saveFeatures("./cosine_features.csv", self.cosineF)
+
+
+    def ranking(self, song):
+        indexSong = -1
+        for i in range(len(self.songsNames)):
+            if self.songsNames[i] == song:
+                indexSong = i
+                break
+        print("\n\n\n" + self.songsNames[indexSong])
+
+        print("\nEUCLIDIAN 100F")
+        first20euclidian100 = np.argsort(self.euclidian100[indexSong , : ])[0:21].astype(int)
+        
+        for pos in first20euclidian100:
+            print(self.songsNames[pos] , end = " , ")
+        
+
+        print("\n\nMANHATTAN 100F")
+        first20manhattan100 = np.argsort(self.manhattan100[indexSong , : ])[0:21].astype(int)
+        for pos in first20manhattan100:
+            print(self.songsNames[pos] , end = " , ")
+
+        print("\n\nCOSINE 100F")
+        first20cosine100 = np.argsort(self.cosine100[indexSong , : ])[0:21].astype(int)
+        for pos in first20cosine100:
+            print(self.songsNames[pos] , end = " , ")
+
+        #-------------------------------------
+
+        print("\n\nEUCLIDIAN F")
+        first20euclidianF = np.argsort(self.euclidianF[indexSong , : ])[0:21].astype(int)
+        for pos in first20euclidianF:
+            print(self.songsNames[pos] , end = " , ")
+
+        print("\n\nMANHATTAN F")
+        first20manhattanF = np.argsort(self.manhattanF[indexSong , : ])[0:21].astype(int)
+        for pos in first20manhattanF:
+            print(self.songsNames[pos] , end = " , ")
+
+        print("\n\nCOSINE F")
+        first20cosineF = np.argsort(self.cosineF[indexSong , : ])[0:21].astype(int)
+        for pos in first20cosineF:
+            print(self.songsNames[pos] , end = " , ")
+         
+            
+                    
 
 if __name__ == "__main__":
     file = feature('./assets/top100_features.csv')
